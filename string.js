99e344d6892574695cf87471e92a005274a93dd2
Sameer Alam <sameeralam316@gmail.com>
2026-09-18 20:15:16 +0530

    string ducat

--- string.js
+++ string.js
@@ -2,7 +2,7 @@
 
 let str = 'sameer'
 let name = "sameer"
-let naam = `sameer`
+let naam = ` sam e e r `
 
 console.log(str,name,naam);
 
@@ -29,13 +29,47 @@ nstr = nstr.concat(naam,name)
 
 console.log(nstr);
 
-let ch = prompt("Enter your string")
-if (ch.startsWith('www')) {
-    document.write(`${ch} `)
-}else if(ch.endsWith('@gmail.com')){
-    console.log(ch);
+// let ch = prompt("Enter your string")
+// if (ch.startsWith('www')) {
+//     document.write(`${ch} `)
+// }else if(ch.endsWith('@gmail.com')){
+//     console.log(ch);
     
-}else{
-    console.log("lol");
+// }else{
+//     console.log("lol");
     
-}
+// }
+
+console.log(naam.toUpperCase());
+console.log(naam.toLowerCase());
+console.log(naam.toLocaleLowerCase());
+console.log(naam.toLocaleUpperCase());
+
+console.log(naam.substring(0,2));
+console.log(naam.substr(0,2));
+console.log(naam.slice(5));
+console.log(naam.repeat(5));
+console.log(naam.replace('', ' '));
+console.log(naam.replaceAll(' ', ''));
+console.log(naam.split('').reverse().join(""));
+console.log(naam.trim());
+
+
+
+// Object
+
+let obj = {
+    name : 'sameer',
+    age : 21,
+    city : 'roorkee'
+
+};
+
+let obj1 = {}
+obj1.naam = 'shamir'
+obj1.age = 21;
+
+let emp = new Object()
+emp.naam = 'sammer'
+
+console.log(obj,obj1,emp);
